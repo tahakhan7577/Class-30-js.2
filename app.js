bd1e89718435e57a508d1941eb90f8300c9fae47
Taha Khan <ktaha7577@gmail.com>
2024-02-28 05:52:16 +0500

    Add Long Hand

---
+++ app.js
@@ -0,0 +1,17 @@
+// Numbers and String(inverted comas ke sath"")
+// "HEllow 123456789"
+
+// Number (Inverted Comas ke Baghair)
+// 123456789
+
+// String(Inverted comas ke Sath "")
+// "abcd and Special characters"
+
+// Long Hand
+
+var greet1 = "Good morning"
+var greet2 = "Good Afternoon"
+var greet3 = "Good evening"
+alert(greet1)
+alert(greet2)
+alert(greet3)
