5e5c6962bff1d7fabe579407f63876f109f358a7
Taha Khan <ktaha7577@gmail.com>
2024-02-28 05:56:06 +0500

    Short Hand

--- app.js
+++ app.js
@@ -9,9 +9,18 @@
 
 // Long Hand
 
+// var greet1 = "Good morning" = 1st
+// var greet2 = "Good Afternoon" = 2nd
+// var greet3 = "Good evening" = 3rc
+// alert(greet1) = 1st
+// alert(greet2) = 2nd
+// alert(greet3) = 3rd
+
+// Short Hand
+
 var greet1 = "Good morning"
-var greet2 = "Good Afternoon"
-var greet3 = "Good evening"
 alert(greet1)
-alert(greet2)
-alert(greet3)
+var greet1 = "Good Afternoon"
+alert(greet1)
+var greet1 = "Good evening"
+alert(greet1)
